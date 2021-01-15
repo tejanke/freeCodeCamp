4032b6aa5613fe7bb6c224768874e5385690387b
tejanke <62154969+tejanke@users.noreply.github.com>
2021-01-15 11:27:31 -0500

    add root route

--- APIs and Microservices Certification/Basic Node and Express/myApp.js
+++ APIs and Microservices Certification/Basic Node and Express/myApp.js
@@ -2,6 +2,10 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function (req, res) {
+    res.send("Hello Express")
+})
+
 console.log("Hello World")
 
 module.exports = app;

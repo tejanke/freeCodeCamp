26e19cd5413a145c22720ef692dbe3415d8dce56
tejanke <62154969+tejanke@users.noreply.github.com>
2021-01-16 12:24:31 -0500

    add route

--- APIs and Microservices Certification/Basic Node and Express/myApp.js
+++ APIs and Microservices Certification/Basic Node and Express/myApp.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send("Hello Express")
+    //res.send("Hello Express")
+    res.sendFile("/views/index.html", { root: __dirname })
 })
 
 console.log("Hello World")

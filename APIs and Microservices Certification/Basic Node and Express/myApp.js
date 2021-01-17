86f640e371de8a78e716692e8f7923f75bd4c9a6
tejanke <62154969+tejanke@users.noreply.github.com>
2021-01-17 11:30:14 -0500

    app use path

--- APIs and Microservices Certification/Basic Node and Express/myApp.js
+++ APIs and Microservices Certification/Basic Node and Express/myApp.js
@@ -2,10 +2,15 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    //res.send("Hello Express")
-    res.sendFile("/views/index.html", { root: __dirname })
+/*
+app.get('/', function (req, res)  {
+  //res.send("Hello Express")
+  res.sendFile("/views/index.html", {root: __dirname})
 })
+*/
+
+path = __dirname + "/public"
+app.use("/", express.static(path))
 
 console.log("Hello World")
 
